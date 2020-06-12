9b368e12e35a528385ce02222f4efbfb0e131c4f
Agustina Osimani <aosimani@itba.edu.ar>
2020-06-12 09:54:48 -0300

    MOD - repeated code deleted

--- embed.c
+++ embed.c
@@ -49,50 +49,39 @@ uint8_t * get_bytes_to_embed(unsigned long * length_bytes_to_embed) {
         exit(EXIT_FAILURE);
     }
 
+    // Get bytes_to_embed length (real size || data from file_to_hide || extension)
+    *length_bytes_to_embed = sizeof(uint32_t) + length_file_to_hide + strlen(extension) + 1;
+    uint8_t * bytes_to_embed = malloc((size_t) *length_bytes_to_embed);
+
+    // Transform length_file_to_hide from decimal to hexa in Big Endian 32 bits representation
+    uint32_t block_size = __bswap_32((unsigned)length_file_to_hide);
+
+    // Generate bytes_to_embed (real size || data from file_to_hide || extension)
+    memcpy(bytes_to_embed, &block_size, sizeof(uint32_t));
+    memcpy(bytes_to_embed + sizeof(uint32_t), buffer_file_to_hide, (size_t)length_file_to_hide);
+    memcpy(bytes_to_embed + sizeof(uint32_t) + length_file_to_hide, extension, strlen(extension) + 1);
+    free(buffer_file_to_hide);
+
     if (stegobmp_config.encrypt) {
-        // Get plain_text length (real size || data from file_to_hide || extension)
-        unsigned long length_plain_text = sizeof(uint32_t) + length_file_to_hide + strlen(extension) + 1;
-        uint8_t * plain_text = malloc((size_t) length_plain_text);
-
-        // Transform length_file_to_hide from decimal to hexa in Big Endian 32 bits representation
-        uint32_t block_size = __bswap_32((unsigned)length_file_to_hide);
-
-        // Generate plain_text (real size || data from file_to_hide || extension)
-        memcpy(plain_text, &block_size, sizeof(uint32_t));
-        memcpy(plain_text + sizeof(uint32_t), buffer_file_to_hide, (size_t)length_file_to_hide);
-        memcpy(plain_text + sizeof(uint32_t) + length_file_to_hide, extension, strlen(extension) + 1);
-
         // Get cipher
         int length_cipher;
-        unsigned char * cipher = encrypt(plain_text, length_plain_text, &length_cipher);
-        free(plain_text);
+        unsigned char * cipher = encrypt(bytes_to_embed, *length_bytes_to_embed, &length_cipher);
+        free(bytes_to_embed);
 
         // Transform length_cipher from decimal to hexa in Big Endian 32 bits representation
         uint32_t cipher_block_size = __bswap_32((unsigned) length_cipher);
 
         // Generate bytes_to_embed (cipher size || cipher(real size || data from file_to_hide || extension))
-        uint8_t * bytes_to_embed = malloc(length_cipher + sizeof(uint32_t));
-        memcpy(bytes_to_embed, &cipher_block_size, sizeof(uint32_t));
-        memcpy(bytes_to_embed + sizeof(uint32_t), cipher, (size_t) length_cipher);
+        uint8_t * cipher_bytes_to_embed = malloc(length_cipher + sizeof(uint32_t));
+        memcpy(cipher_bytes_to_embed, &cipher_block_size, sizeof(uint32_t));
+        memcpy(cipher_bytes_to_embed + sizeof(uint32_t), cipher, (size_t) length_cipher);
         free(cipher);
         free(buffer_file_to_hide);
 
         *length_bytes_to_embed = length_cipher + sizeof(uint32_t);
-        return bytes_to_embed;
+        return cipher_bytes_to_embed;
 
     } else {
-        *length_bytes_to_embed = sizeof(uint32_t) + length_file_to_hide + strlen(extension) + 1;
-
-        // Transform length_file_to_hide from decimal to hexa in Big Endian 32 bits representation
-        uint32_t block_size = __bswap_32((unsigned)length_file_to_hide);
-
-        // Generate bytes_to_embed (real size || data from file_to_hide || extension)
-        uint8_t * bytes_to_embed = malloc(*length_bytes_to_embed);
-        memcpy(bytes_to_embed, &block_size, sizeof(uint32_t));
-        memcpy(bytes_to_embed + sizeof(uint32_t), buffer_file_to_hide, (size_t) length_file_to_hide);
-        memcpy(bytes_to_embed + sizeof(uint32_t) + length_file_to_hide, extension, strlen(extension) + 1);
-
-        free(buffer_file_to_hide);
 
         return bytes_to_embed;
     }
